4a1c23d66b571fc442a03f599e1b8f37cf555e7c
Manuel Romero <manuel.romero@qlik.com>
2018-01-26 09:49:03 +0100

    Using pushd and popd

--- init-shared-content.sh
+++ init-shared-content.sh
@@ -2,8 +2,8 @@
 
 # Public scripts
 git clone https://github.com/Qlik-Partner-Tools/qlik-automation-core.git shared-content
-cd shared-content
+pushd shared-content
 git checkout .
 git pull origin master
-cd ..
+popd
 
